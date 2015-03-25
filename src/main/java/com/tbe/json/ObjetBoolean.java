c8e1f96e2f87db3eaaafcc3f5030b2df74926b84
ZACHE NJIEDJOUK <njiedjoz@bouleau19.iut-infobio.priv.univ-lille1.fr>
2015-03-25 10:53:57 +0100

    db suite

---
+++ src/main/java/com/tbe/json/ObjetBoolean.java
@@ -0,0 +1,23 @@
+package com.tbe.json;
+
+
+
+public class ObjetBoolean {
+	
+	private boolean bool;
+
+	public ObjetBoolean() {}
+	
+	public ObjetBoolean(boolean bool) {
+		this.bool=bool;
+	}
+
+	public boolean isBool() {
+		return bool;
+	}
+
+	public void setBool(boolean bool) {
+		this.bool = bool;
+	}
+	
+}
---
+++ src/main/java/com/tbe/json/ObjetjSON.java
@@ -0,0 +1,28 @@
+package com.tbe.json;
+
+
+public class ObjetjSON {
+	private String login;
+	private String password;
+
+	
+	public ObjetjSON() {}
+	
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}	
+	
+}
